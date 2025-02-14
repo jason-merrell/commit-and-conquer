b2ff119144221409771ae80a0a09abbe103beb71
Jason Merrell <jason@merrellworks.com>
2025-02-14 13:34:24 -0600

    refactor layout and styles; update creature images and add new creature card

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import creatures from "@/data/creatures.json"
 
 export default function Page() {
   return (
-    <div className="flex flex-wrap gap-2 items-center justify-center min-h-screen bg-gradient-to-r from-cyan-900 to-pink-900 p-4">
+    <div className="flex flex-wrap gap-2 items-center justify-center min-h-screen bg-gradient-to-r from-gray-700 to-gray-900 p-4">
       {
         creatures.map((creature)=> <CreatureCard key={creature.id} data={creature} />)
       }
