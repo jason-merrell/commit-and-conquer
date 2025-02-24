b9448e3bba9e4dadf65ef6202138d813b52e882d
Jason Merrell <jason@merrellworks.com>
2025-02-24 13:41:15 -0600

    reverse creature display order in the main page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className="flex flex-wrap gap-2 items-center justify-center min-h-screen bg-gradient-to-r from-gray-700 to-gray-900 p-4">
       {
-        creatures.map((creature)=> <CreatureCard key={creature.id} data={creature} />)
+        creatures.slice().reverse().map((creature) => <CreatureCard key={creature.id} data={creature} />)
       }
     </div>
   )
